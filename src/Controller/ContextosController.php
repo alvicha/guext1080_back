7559d95b94a39553f505510d5a6ec4209656c301
Alberto Villegas <alvich@floridauniversitaria.es>
2025-05-18 18:07:24 +0200

    Corrigiendo codigo backend

--- src/Controller/ContextosController.php
+++ src/Controller/ContextosController.php
@@ -2,12 +2,9 @@
 
 namespace App\Controller;
 
-use App\Entity\Contextos;
 use App\Repository\ContextosRepository;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
